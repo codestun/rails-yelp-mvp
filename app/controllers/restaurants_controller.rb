187f2a40d04076d407b3bef1b73d5cbb2674de57
Doerthe Kosicki <kiki@codestun.com>
2020-08-12 14:46:52 +0200

    update restaurant controller

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,2 +1,38 @@
 class RestaurantsController < ApplicationController
+  before_action :find_restaurant, except: [:index, :new, :create]
+  
+  def index
+    @restaurants = Restaurant.all
+    render
+  end
+
+  def show
+  end
+
+  def new
+    @restaurant = Restaurant.new
+  end
+
+  def edit
+  end
+
+  def create
+    @restaurant = Restaurant.new(restaurant_params)
+
+    if @restaurant.save
+      redirect_to @restaurant, notice: "Restaurant was created successfully!"
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def find_restaurant
+    @restaurant = Restaurant.find(params[:id])
+  end
+
+  def restaurant_params
+    params.require(:restaurant).permit(:name, :address, :phone_number, :category)
+  end
 end
