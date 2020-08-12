a0fa38f76359192d71ad94383786159cf73440bf
Doerthe Kosicki <kiki@codestun.com>
2020-08-12 12:51:28 +0200

    Update model restaurant

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+puts "Cleaning database..."
+Restaurant.destroy_all
+
+puts "Creating some restaurants..."
+50.times do
+  restaurant = Restaurant.create(
+    name: Faker::Company.name,
+    address: Faker::Address.street_address,
+    phone_number: Faker::PhoneNumber.phone_number,
+    category: ["chinese", "italian", "japanese", "french", "belgian"].sample
+    )
+  puts "Restaurant with id #{restaurant.id} was created"
+
+end
+
+puts "Created #{Restaurant.count} restaurants"
